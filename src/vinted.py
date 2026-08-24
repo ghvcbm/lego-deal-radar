57581642bfa07383fd9509c8a3abcc7f1607453d
ghvcbm <ghvcbm1@gmail.com>
2026-08-24 21:31:39 +0200

    Create vinted.py

---
+++ src/vinted.py
@@ -0,0 +1,12 @@
+from marketplace import Marketplace, Listing
+
+
+class VintedMarketplace(Marketplace):
+
+    @property
+    def name(self) -> str:
+        return "vinted"
+
+    def search(self, query: str) -> list[Listing]:
+        # TODO: implémenter la recherche Vinted
+        return []
